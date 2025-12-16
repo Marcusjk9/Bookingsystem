40e394591391ad41c42bdefedaa28eca9aa85064
github username <dusan.todorovic@student.nbi-handelsakademin.se>
2025-12-16 11:00:28 +0100

    admin.cs modified, login.cs modified, program.cs modified, md files modified, employeeslogin deprecieted

--- login.cs
+++ login.cs
@@ -34,4 +34,54 @@ static class Login
 
         return result;
     }
+
+    public static async Task<bool>   // admin login metod
+    PostAdmin(Post_Args credentials, Config config, HttpContext ctx)
+    {
+        bool result = false;
+        string query = "SELECT id, role FROM admins WHERE email = @email AND password = @password";
+        var parameters = new MySqlParameter[]
+        {
+            new("@email", credentials.Email),
+            new("@password", credentials.Password),
+        };
+
+        using var reader = await MySqlHelper.ExecuteReaderAsync(config.db, query, parameters);
+        if (await reader.ReadAsync())
+        {
+            int id = reader.GetInt32(0);
+            string role = reader.GetString(1);
+
+            if (ctx.Session.IsAvailable)
+            {
+                ctx.Session.SetInt32("admin_id", id);
+                ctx.Session.SetString("admin_role", role);
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    public static async Task<bool>   // employee login 
+    PostEmployee(Post_Args credentials, Config config, HttpContext ctx)
+    {
+        bool result = false;
+        string query = "SELECT id FROM employees WHERE email = @email AND password = @password";
+        var parameters = new MySqlParameter[]
+        {
+            new("@email", credentials.Email),
+            new("@password", credentials.Password),
+        };
+
+        object query_result = await MySqlHelper.ExecuteScalarAsync(config.db, query, parameters);
+        if(query_result is int id)
+        {
+            if(ctx.Session.IsAvailable)
+            {
+                ctx.Session.SetInt32("employee_id", id);
+                result = true;
+            }
+        }
+        return result;
+    }
 }
